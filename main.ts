e9ec07523eddd4fa631bf4877842f3223f6e493b
eschlot <eschlot@users.noreply.github.com>
2021-01-02 01:54:12 +0100

    First functional, hard to play racer game.

--- main.ts
+++ main.ts
@@ -10,11 +10,44 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.WandTeilRechts, function (sprite
     Rennauto.x += -10
     info.changeLifeBy(-1)
 })
+
+
+function calcWalls (offset: number) {
+
+    for (let Index3 = AnzahlWandteile; Index3 >0 ; Index3--) {
+            linkeWandTeil = linkeWandListe[Index3]
+            rechteWandTeil = rechteWandListe[Index3]
+
+            linkeWandTeil.x = linkeWandListe[Index3-1].x
+            rechteWandTeil.x = rechteWandListe[Index3-1].x
+        }
+
+    rechteWandTeil = rechteWandListe[0]
+    linkeWandTeil = linkeWandListe[0]
+
+    let newX= linkeWandTeil.x + offset
+
+    if (newX > -linkeWandTeil.width+strassenbreite && newX <linkeWandTeil.width-strassenbreite-strassenbreite)
+    {
+        let posR = newX+linkeWandTeil.width+strassenbreite
+        console.logValue("newX", newX)
+        console.logValue("posR", posR)
+
+        linkeWandTeil.x = newX
+        rechteWandTeil.x = posR
+    }
+}
+
+
+let updateExecutionCtr =0
+let kurvenzufallszahl = 0
 let kurve = 0
-let kurvenzuffallszahl = 0
-let rechteWandTeil: Sprite = null
-let linkeWandTeil: Sprite = null
+let strassenbreite = 70
+let rechteWandListe: Sprite[] = []
+let linkeWandListe: Sprite[] = []
 let Rennauto: Sprite = null
+let linkeWandTeil: Sprite = null
+let rechteWandTeil: Sprite = null
 Rennauto = sprites.create(img`
     . . . . . . e e c c e e . . . . 
     . . . . . e 2 2 2 2 2 2 e . . . 
@@ -34,85 +67,75 @@ Rennauto = sprites.create(img`
     . . . . f f . . . . . . f f . . 
     `, SpriteKind.Player)
 Rennauto.setFlag(SpriteFlag.StayInScreen, true)
-let linkeWandListe = sprites.allOfKind(SpriteKind.WandTeilLinks)
-let rechteWandListe = sprites.allOfKind(SpriteKind.WandTeilRechts)
 controller.moveSprite(Rennauto, 100, 100)
-let AnzahlWandteile = 8
+linkeWandListe = sprites.allOfKind(SpriteKind.WandTeilLinks)
+rechteWandListe = sprites.allOfKind(SpriteKind.WandTeilRechts)
+let AnzahlWandteile = 24
 for (let Index = 0; Index <= AnzahlWandteile; Index++) {
     linkeWandTeil = sprites.create(img`
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        `, SpriteKind.WandTeilLinks)
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+    `, SpriteKind.WandTeilLinks)
     linkeWandListe.push(linkeWandTeil)
-    linkeWandTeil.setPosition(0, 0 + Index * 16)
-}
-for (let Index = 0; Index <= AnzahlWandteile; Index++) {
     rechteWandTeil = sprites.create(img`
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        7777777777777777777777777777777777777777777777777777777777777777
-        `, SpriteKind.WandTeilRechts)
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+    `, SpriteKind.WandTeilRechts)
     rechteWandListe.push(rechteWandTeil)
-    rechteWandTeil.setPosition(160, 0 + Index * 16)
+
+    let posL = -50 
+    let posR = posL+linkeWandTeil.width+strassenbreite
+    let y = Index * 120 / AnzahlWandteile
+    linkeWandTeil.setPosition(posL, y)
+    rechteWandTeil.setPosition(posR, y)
+
 }
 info.setLife(6)
-game.onUpdateInterval(1000, function () {
-    kurvenzuffallszahl = randint(-1, 1)
-    if (0.3 < kurvenzuffallszahl) {
+
+
+game.onUpdateInterval(10,function () {
+
+    updateExecutionCtr++
+
+    let speed = ((Rennauto.y-50)*3) / 120 
+    if (updateExecutionCtr>=speed)
+    {   
+        updateExecutionCtr=0
+        if (1 == kurve) {
+            calcWalls(3)
+        } else {
+            if (-1 == kurve) {
+                calcWalls(-3)
+            } else {
+                calcWalls(0)
+            }
+        }
+    }
+})
+
+game.onUpdateInterval(500, function () {
+    kurvenzufallszahl = randint(-1, 1)
+    if (0.2 < kurvenzufallszahl) {
         kurve = 1
     } else {
-        if (-0.3 > kurvenzuffallszahl) {
+        if (-0.2 > kurvenzufallszahl) {
             kurve = -1
         } else {
             kurve = 0
         }
     }
 })
-game.onUpdateInterval(100, function () {
-    if (1 == kurve) {
-        for (let Index = 0; Index <= AnzahlWandteile; Index++) {
-            rechteWandTeil = rechteWandListe[Index]
-            rechteWandTeil.x += 3
-            linkeWandTeil = linkeWandListe[Index]
-            linkeWandTeil.x += 3
-        }
-    } else {
-        if (-1 == kurve) {
-            for (let Index = 0; Index <= AnzahlWandteile; Index++) {
-                rechteWandTeil = rechteWandListe[Index]
-                rechteWandTeil.x += -3
-                linkeWandTeil = linkeWandListe[Index]
-                linkeWandTeil.x += -3
-            }
-        } else {
-        	
-        }
+
+game.onUpdateInterval(2500, function () {
+    if (strassenbreite>60)
+    {
+        strassenbreite=strassenbreite-1
     }
+    info.changeScoreBy(1)
 })

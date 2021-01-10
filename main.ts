ff5291b8354f23c6854834cbc210f40dc97b3bf9
eschlot <eschlot@users.noreply.github.com>
2021-01-10 18:43:29 +0100

    Functional again with a little bit of effect on the graphics

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace SpriteKind {
     export const Street = SpriteKind.create()
 }
 
-// Standard palette
+// enum Color
 enum Color {
     Transparent, // 0
     White, // 1 = RGB(255, 255, 255)
@@ -22,35 +22,27 @@ enum Color {
     Bone, // 13 = RGB(229, 205, 196)
     Brown, // 14 = RGB(145, 70, 61)
     Black // 15 = RGB(0, 0, 0)
-}   // enum Color
+}
 
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.WandTeilLinks, function (sprite, otherSprite) {
     Rennauto.x += 10
     scene.cameraShake(4, 300)
+    controller.vibrate(200)
     info.changeLifeBy(-1)
 })
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.WandTeilRechts, function (sprite, otherSprite) {
     Rennauto.x += -10
     scene.cameraShake(4, 300)
+    controller.vibrate(200)
     info.changeLifeBy(-1)
 })
 
-function calcLine(newX: number, linkeWandTeil: Sprite, rechteWandTeil: Sprite, streetSprite: Sprite)
-{
-    let streetPos = newX + linkeWandTeil.width
-    let posR =  streetPos + strassenbreite
-    
-    linkeWandTeil.x = newX
-    rechteWandTeil.x = posR
-    streetSprite.x = streetPos
-
-    console.logValue("linkeWandTeil.x",linkeWandTeil.x)
-    console.logValue("rechteWandTeil.x",rechteWandTeil.x)
-    console.logValue("streetSprite.x",streetSprite.x)
-
-
+function calcLine (pos: number, linkeWandTeil: Sprite, rechteWandTeil: Sprite, streetSprite: Sprite) {
+    streetSprite.x = pos
+    linkeWandTeil.right = pos-streetOffset
+    rechteWandTeil.left = pos+streetOffset
 }
 
 function calcWalls (offset: number) {
@@ -59,8 +51,8 @@ function calcWalls (offset: number) {
             rechteWandTeil = rechteWandListe[Index3]
             streetSprite = streetListe[Index3]
 
-            linkeWandTeil.x = linkeWandListe[Index3-1].x
-            rechteWandTeil.x = rechteWandListe[Index3-1].x
+            linkeWandTeil.right = linkeWandListe[Index3-1].right
+            rechteWandTeil.left = rechteWandListe[Index3-1].left
             streetSprite.x = streetListe[Index3-1].x
         }
 
@@ -68,119 +60,122 @@ function calcWalls (offset: number) {
     linkeWandTeil = linkeWandListe[0]
     streetSprite = streetListe[0]
 
-    newX = linkeWandTeil.x + offset
-    if (newX > 0 - linkeWandTeil.width + strassenbreite && 
-        newX < linkeWandTeil.width - strassenbreite - strassenbreite) 
-    {
-        calcLine(newX,linkeWandTeil,rechteWandTeil,streetSprite)
+    streetOffset = Math.floor(strassenbreite/2)
+    pos = streetSprite.x + offset
+
+    if ((pos > streetOffset) && (pos < scene.screenWidth()-streetOffset )) {
+        calcLine(pos, linkeWandTeil, rechteWandTeil, streetSprite)
     }
 }
 
 
-let scoreAddition = 0
 let kurvenzufallszahl = 0
 let kurve = 0
-let updateExecutionCtr = 0
-let posR = 0
-let newX = 0
-let posR2 = 0
-let posL = 0
-let streetSprite: Sprite = null
+let streetOffset=0
+let streetPos = 0
+let pos = 50
+
+
+let Rennauto: Sprite = null
+
 let streetImage: Image = null
 let tempLw: Image = null
-let Rennauto: Sprite = null
+
+let rechteWandTeil: Sprite = null
+let linkeWandTeil: Sprite = null
+let streetSprite: Sprite = null
+
+let streetListe: Sprite[] = []
+let linkeWandListe: Sprite[] = []
+let rechteWandListe: Sprite[] = []
 
 let startStrassenbreite = 90
 let strassenbreite = startStrassenbreite
-let rechteWandListe: Sprite[] = []
-let linkeWandListe: Sprite[] = []
-let linkeWandTeil: Sprite = null
-let rechteWandTeil: Sprite = null
-let speed = 5
+
+let scoreAddition = 0
+let speed = 30
+let controllerTopOffset = 80
 
 
-
-Rennauto = sprites.create(img`
-    . . . . . . e e c c e e . . . . 
-    . . . . . e 2 2 2 2 2 2 e . . . 
-    . . . . 2 c 2 2 2 2 2 2 c 2 . . 
-    . . . e 2 c 4 2 2 2 2 2 c 2 e . 
-    . . . f 2 2 4 2 2 2 2 2 c 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 c 2 4 4 2 2 2 c 2 f . 
-    . . . e 2 c e c c c c e c 2 e . 
-    . . . e 2 e c b b b b c e 2 e . 
-    . . . e 2 e b b b b b b e 2 e . 
-    . . . e e e e e e e e e e e e . 
-    . . . f e d e e e e e e d e f . 
-    . . . f e 2 d e e e e d 2 e f . 
-    . . . f f e e e e e e e e f f . 
-    . . . . f f . . . . . . f f . . 
-    `, SpriteKind.Player)
-Rennauto.setFlag(SpriteFlag.StayInScreen, true)
-Rennauto.y = 120
-controller.moveSprite(Rennauto, 100, 20)
 linkeWandListe = sprites.allOfKind(SpriteKind.WandTeilLinks)
 rechteWandListe = sprites.allOfKind(SpriteKind.WandTeilRechts)
-let streetListe = sprites.allOfKind(SpriteKind.Street)
-let AnzahlWandteile = 12
+streetListe = sprites.allOfKind(SpriteKind.Street)
+
+let AnzahlWandteile = 13
 let hoehe = 1
 let y = 20
+
+pos = scene.screenWidth() /2
+streetOffset = Math.floor(strassenbreite/2)
+
 for (let Index = 0; Index <= AnzahlWandteile; Index++) {
-    tempLw = image.create(scene.screenWidth(), hoehe+1)
+    tempLw = image.create(scene.screenWidth(), hoehe)
     tempLw.fill(Color.BrightGreen)
-    streetImage = image.create(startStrassenbreite, hoehe+1)
-    streetImage.fill(Color.Black)
+    
+    streetImage = image.create(startStrassenbreite, hoehe)
+    streetImage.fill(Color.Bone)
     if (Index % 2 == 0) {
         streetImage.drawLine(startStrassenbreite / 2, 0, startStrassenbreite / 2, hoehe + 1, Color.White)
     }
-
-    streetSprite = sprites.create(streetImage, SpriteKind.Street)
-    streetListe.push(streetSprite)
     linkeWandTeil = sprites.create(tempLw, SpriteKind.WandTeilLinks)
     linkeWandListe.push(linkeWandTeil)
     rechteWandTeil = sprites.create(tempLw, SpriteKind.WandTeilRechts)
     rechteWandListe.push(rechteWandTeil)
+    streetSprite = sprites.create(streetImage, SpriteKind.Street)
+    streetListe.push(streetSprite)
+    
+    linkeWandTeil.y = y
+    rechteWandTeil.y = y
+    streetSprite.y = y
+    calcLine(pos, linkeWandTeil, rechteWandTeil, streetSprite)
 
-    calcLine(newX,linkeWandTeil,rechteWandTeil,streetSprite)
-
-    linkeWandTeil.y=y
-    rechteWandTeil.y=y
-    streetSprite.y=y
-
-    posL = -50
-    calcLine(posL, linkeWandTeil, rechteWandTeil, streetSprite)
     y = y + hoehe
     hoehe += 1
 }
+Rennauto = sprites.create(img`
+    . . . . . . e e c c e e . . . .
+    . . . . . e 2 2 2 2 2 2 e . . .
+    . . . . 2 c 2 2 2 2 2 2 c 2 . .
+    . . . e 2 c 4 2 2 2 2 2 c 2 e .
+    . . . f 2 2 4 2 2 2 2 2 c 2 f .
+    . . . f 2 2 4 2 2 2 2 2 2 2 f .
+    . . . f 2 2 4 2 2 2 2 2 2 2 f .
+    . . . f 2 c 2 4 4 2 2 2 c 2 f .
+    . . . e 2 c e c c c c e c 2 e .
+    . . . e 2 e c b b b b c e 2 e .
+    . . . e 2 e b b b b b b e 2 e .
+    . . . e e e e e e e e e e e e .
+    . . . f e d e e e e e e d e f .
+    . . . f e 2 d e e e e d 2 e f .
+    . . . f f e e e e e e e e f f .
+    . . . . f f . . . . . . f f . .
+`, SpriteKind.Player)
+Rennauto.setFlag(SpriteFlag.StayInScreen, true)
+Rennauto.y = 110
+controller.moveSprite(Rennauto, 100, 20)
 info.setLife(6)
-let controllerTopOffset = 90
 game.onUpdateInterval(2500, function () {
-    if (strassenbreite > 40) {
+    if (strassenbreite > 45) {
         strassenbreite = strassenbreite - 1
     }
 })
-game.onUpdateInterval(10, function () {
-    speed = Math.floor((Rennauto.y - controllerTopOffset) / 10)
-    if (speed < 0) {
-        speed = 0
+forever(function () {
+    if (controllerTopOffset > Rennauto.y) {
+        Rennauto.y = controllerTopOffset
     }
-    // console.logValue("speed", speed)
-    // console.logValue("Rennauto.y",Rennauto.y)
-    if (updateExecutionCtr >= speed) {
-        updateExecutionCtr = 0
-        if (1 == kurve) {
-            calcWalls(3)
-        } else {
-            if (-1 == kurve) {
-                calcWalls(-3)
-            } else {
-                calcWalls(0)
-            }
-        }
+    speed = Rennauto.y - controllerTopOffset
+    if (speed < 10) {
+        speed = 10
+    }
+    pause(speed)
+    if (1 == kurve) {
+        calcWalls(3)
     } else {
-        updateExecutionCtr += 1
+        if (-1 == kurve) {
+            calcWalls(-3)
+        } else {
+            calcWalls(0)
+        }
     }
 })
 game.onUpdateInterval(500, function () {
@@ -194,6 +189,6 @@ game.onUpdateInterval(500, function () {
             kurve = 0
         }
     }
-    scoreAddition = 10 - speed
+    scoreAddition = scene.screenHeight() - controllerTopOffset - speed
     info.changeScoreBy(scoreAddition)
 })
